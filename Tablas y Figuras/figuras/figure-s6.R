2d177ac0aba9d102869fc3d9115228d8c5dfb9d4
Stephanie M. Ramos Camacho <78103316+stephanie-ramosc@users.noreply.github.com>
2021-12-15 21:20:06 -0400

    figure-s6

---
+++ Tablas y Figuras/figuras/figure-s6.R
@@ -0,0 +1,42 @@
+#Stephanie M. Ramos Camacho
+#Figure S6
+
+#analizar las muertes cercana a vecinos por strata
+
+source("/Users/stephanie.marie/Documents/Semestre 1 2021-2022/CCOM 3031/Proyecto Final_Huracán María/base.R")
+
+#libreria a usarse
+lib_eval("tidyverse")
+
+households <- readRDS("/Users/stephanie.marie/Documents/Semestre 1 2021-2022/CCOM 3031/Proyecto Final_Huracán María/hh_main.RDS")
+
+#limpiando la data para el analisis de los vecinos
+households$mort_neighbor_f <- ordered(households$mort_neighbor,
+                                      levels = c(99,0,1,2,3,4,5,6),
+                                      labels = c("No se sabe", 
+                                                 "0","1","2","3","4","5", "7"))
+households$neighbors_num_f <- ordered(households$neighbors_num,
+                                      levels = c(1,2,3,4,5),
+                                      labels = c(9, 25, 50, 100, 200))
+
+#variable para hacer la grafica con ggplot de las defunciones
+figs6 <- households %>%
+  subset(!mort_neighbor == "---")%>% 
+  mutate(n_rate = as.numeric(as.character(mort_neighbor_f))/as.numeric(as.character(neighbors_num_f))) %>%
+  mutate(n_rate = ifelse(is.na(n_rate), 0, n_rate)) %>%
+  {aggregate(n_rate~strata, data = ., FUN = mean)} %>%
+  mutate(n_rate = n_rate*1000) %>% 
+  ggplot(aes(y=n_rate, x=strata)) + 
+  geom_bar(stat="identity") + 
+  scale_x_discrete("Lejanía/Distancia", limits=c(1:8), 
+                   labels=c("1 - Menor Remoto", "2", "3", "4",
+                            "5", "6", "7", "8 - Mayor Remoto")) +
+  ylab("Número de Defunciones por cada 1,000 Personas") + 
+  #ggtitle
+  theme_classic() + 
+  geom_hline(yintercept = 14.4, color="red", linetype =2) + 
+  geom_label(aes(0,14.4,label = "Tasa Estimada de Mortalidad después de un Huracán", vjust = -1,hjust=.1))
+
+#guarda y de ser necesario reescribe el pdf
+ggsave("figure-s6.pdf", figs6, "pdf", "/Users/stephanie.marie/Documents/Semestre 1 2021-2022/CCOM 3031/Proyecto Final_Huracán María/",
+       units="in", width=3, height = 3, scale=2)
